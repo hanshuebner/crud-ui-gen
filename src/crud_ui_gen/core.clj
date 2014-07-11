5696053d98476807b28cd826883364c486784df1
Ulrich Becker <ub@kassapo.com>
2014-07-11 16:07:09 +0200

    transform to list of tags

--- src/crud_ui_gen/core.clj
+++ src/crud_ui_gen/core.clj
@@ -3,28 +3,22 @@
             [clojure.walk :as walk]
             [clojure.zip :as zip]))
 
-(defn transform [e tree]
-  (println
+(defn transform [e tree acc]
+  (cons
     (cond
       (= (:tag e) :html) (dissoc e :content)
       (:tag e) (dissoc e :content)
-      :else e)))
+      :else e)
+    acc))
 
 (defn walk [tree]
-  (loop [tree tree]
-    (transform (zip/node tree) tree)
+  (loop [tree tree
+         acc nil]
     (if (zip/end? tree)
-      (zip/root tree)
-      (recur (zip/next tree)))))
-
-(defn print-content [tree]
-  (loop [tree tree]
-    (println (:content (first (zip/next tree))))
-    (if (zip/end? tree)
-      'end
-      (recur (zip/next tree)))))
+      acc
+      (recur (zip/next tree)
+        (transform (zip/node tree) tree acc)))))
 
 (let [html (xml/parse (java.io.FileReader. "a.html"))]
   (walk (zip/xml-zip html)))
-
 
