cb6c6df5e2753ccb449ca1bee636748b40ec62bf
Ulrich Becker <ub@kassapo.com>
2014-07-11 12:29:37 +0200

    lein new

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject crud-ui-gen "2014.1.0-SNAPSHOT"
+  :description "Generate JavaScript from xhtml"
+  :url "undefined"
+  :license {:name "GPL"
+            :url "http://gnu.org/"}
+  :dependencies [[org.clojure/clojure "1.5.1"])
---
+++ test/crud_ui_gen/core_test.clj
@@ -0,0 +1,7 @@
+(ns crud-ui-gen.core-test
+  (:require [clojure.test :refer :all]
+            [crud-ui-gen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
